4d9bb16501548970151236eb283ba05721275df6
jiaodongdong <1183246192@qq.com>
2017-09-25 10:43:43 +0800

    测试提交

---
+++ MyTest/src/Calculator.java
@@ -0,0 +1,29 @@
+
+public class Calculator {
+	public static void main(String[] args) {
+		/*int[] a = {
+				196,196,193,195,195,195,195,195,195,195,197
+		};
+		int count = 0;
+		for (int i = 0; i < a.length; i++) {
+			count = a[i] + count ;
+		}
+		System.out.println("总计："+count);
+		System.out.println("共："+a.length+"个");*/
+		
+		int[] b = {
+				27,36,20,34,13,13,16,17,25,27,31,20,46,13,13
+		};
+		int count1 = 0;
+		for (int i = 0; i < b.length; i++) {
+			count1 = b[i] + count1 ;
+		}
+		System.out.println("总计："+count1);
+		System.out.println("共："+b.length+"个");
+		
+//		System.out.println("一共："+(count+count1-34-21));
+	}
+	
+	
+	
+}
